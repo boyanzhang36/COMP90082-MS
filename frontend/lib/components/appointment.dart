25644f015bb38b6556700f2bbe7003e800d748ab
Shixin Wnag <j2722649746@gmail.com>
2020-05-27 18:58:51 +1000

    Appointment Page Notification

--- frontend/lib/components/appointment.dart
+++ frontend/lib/components/appointment.dart
@@ -2,17 +2,23 @@ class Appointment {
   final DateTime date;
   final String title;
   final String note;
+  final int duration;
+  final String status;
 
   Appointment(
       {this.date,
         this.title,
-        this.note});
+        this.note,
+      this.duration,
+      this.status});
 
   factory Appointment.fromJson(Map<String, dynamic> json) {
     return Appointment(
         date: DateTime.parse(json['date']),
         title: json['title'],
-        note: json['note']
+        note: json['note'] as String,
+        duration: json['duration'] as int,
+        status: json['status']
     );
   }
 }
